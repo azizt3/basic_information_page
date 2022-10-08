dea906460dabf208cf8014b51db93228f6416a63
azizt3 <aziz.tabbish@gmail.com>
2022-10-08 12:48:25 -0400

    feature: added about/contact-me pages to server.

--- index.js
+++ index.js
@@ -9,17 +9,63 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-    // gives full file path of file hosted on server
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+        (err, content) => {
+        if (err) throw err
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(content)
 
-    let filePath = path.join( __dirname, 'basic_information_page',
-    req.url==='/' ? 'index.html' : req.url);
+        })
 
-    // gives extension name (.html, .css, etc)
+    } else if (req.url === '/about.html') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'),
+        (err, content) => {
+        if (err) throw err
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(content)
+        })
 
-    let extName = path.extname(filePath);
+    } else if (req.url === '/contact-me.html') {
+        fs.readFile(path.join(__dirname, 'public', 'contact-me.html'),
+        (err, content) => {
+        if (err) throw err
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(content)
+        })
+    
+    } else {
+        fs.readFile(path.join(__dirname, 'public', '404.html'),
+        (err, content) => {
+        if (err) throw err
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(content)
+        })
+    }
+    });
 
-    console.log(extName);
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+        
+        
+        
+        
 
+
+    
+        
+
+        
+    
+
+    // let filePath = path.join( __dirname, 'basic_information_page',
+    // (req.url === '/') ? 'index.html' : req.url);
+ 
+    // // gives extension name (.html, .css, etc)
+
+    // let extName = path.extname(filePath);
+
+    // let contentType ='text/html';
 
 // Server reads the file and displays some content. If the file does not exist, then
 // an error (err) is shown. When this happens, it automatically opens 404.html
@@ -27,19 +73,21 @@ const server = http.createServer((req, res) => {
 // would be 200 despite there being an error. This is because we are asking the server to
 // put up the 404.html page. So technically, the request did go through.
 
-fs.readFile(filePath,  (err,content) =>{
-    if (err){
+// fs.readFile(filePath,  (err, content) =>{
+//     if (err){
 
-        fs.readFile(path.join(__dirname, 'basic_information_page', '404.html'),
-        (err,content) => {
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.end(content, 'utf8');
+//         fs.readFile(path.join(__dirname, 'basic_information_page', '404.html'),
+//         (err,content) => {
+//             res.writeHead(200, {'Content-Type': 'text/html'});
+//             res.end(content, 'utf8');
 
-        })
+//         })
     
-    };
-});
-});
+//     } else {
 
-const PORT = 8080;
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+//         res.writeHead(200, {'Content-Type': contentType });
+//         res.end(content, 'utf8');
+//     }
+
+// });
+
